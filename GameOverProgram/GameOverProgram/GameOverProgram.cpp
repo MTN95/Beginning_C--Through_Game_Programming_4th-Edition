57010546f2cee78b80f799665a6e698eeba3f502
MTN95 <113786893+MTN95@users.noreply.github.com>
2022-09-23 18:26:04 +0300

    Update GameOverProgram.cpp

--- GameOverProgram/GameOverProgram/GameOverProgram.cpp
+++ GameOverProgram/GameOverProgram/GameOverProgram.cpp
@@ -1,6 +1,34 @@
-// Game over 
-// A first C++ Program
-#include <iostream>
+// Game Over 
+// A First C++ Program
+
+/*
+*****Including Other Files*****
+The next line in the program is a preprocessor directive. You know this
+because the line begins with the # symbol.
+*/
+#include <iostream> // Header File - Including the IO Stream library. IO stands for Input-Output
+
+// Defining The Main Function
+/*
+A function is a group of programming code that can do some work and return
+a value. In this case, int indicates that the function will return an integer
+value. All function headers have a pair of parentheses after the function name.
+
+NOTE: 
+All C++ programs must have a function called main(), which is the starting
+point of the program. The real action begins here.
+
+The next line marks the beginning of the function.
+{
+And the very last line of the program marks the end of the function.
+}
+
+All functions are delimited by a pair of curly braces, and everything between
+them is part of the function. Code between two curly braces is called a block
+and is usually indented to show that it forms a unit. The block of code that
+makes up an entire function is called the body of the function.
+*/
+
 
 int main()
 {
